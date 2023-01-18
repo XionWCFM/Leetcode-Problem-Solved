ed3ffc1c94694ff2f5c167235f325ab320977815
xion <101111364+XionWCFM@users.noreply.github.com>
2023-01-19 07:12:57 +0900

    Time: 57 ms (95.37%), Space: 42.5 MB (17.76%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    let [left,right,middle] = [0,nums.length-1,nums.length/2]
+    return nums.indexOf(target)
+    
+};
