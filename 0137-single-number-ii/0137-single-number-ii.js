f804e5ce822a888921d88f37d4a59020163a5298
xion <101111364+XionWCFM@users.noreply.github.com>
2023-01-19 07:26:55 +0900

    Time: 69 ms (82.38%), Space: 43.3 MB (50.12%) - LeetHub

---
+++ 0137-single-number-ii/0137-single-number-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let map = new Map()
+    
+    for(i=0 ; i<nums.length; i++) {
+        map.set(nums[i] , (map.get(nums[i]) || 0 ) +1 )
+        if(map.get(nums[i]) === 3) {
+            map.delete(nums[i])
+        }
+    }
+    return [...map][0][0]
+};
