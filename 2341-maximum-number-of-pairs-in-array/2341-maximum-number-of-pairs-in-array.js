16a0ebbfd68d6570e3320d795a08b2ef3016c24f
xion <101111364+XionWCFM@users.noreply.github.com>
2023-02-03 00:54:11 +0900

    Time: 54 ms (98.25%), Space: 42.5 MB (45.41%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.js
@@ -4,15 +4,15 @@
  */
 var numberOfPairs = function(nums) {
     let map = new Map()
-    let answer = [0]
+    let answer = 0
     for(i=0; i<nums.length ;i++) {
         let val = map.get(nums[i]) 
         map.set(nums[i], (val||0) +1 )
         if(map.get(nums[i]) === 2) {
-            answer[0]++
+            answer++
             map.delete(nums[i])
         }
     }
-    answer.push(map.size)
-    return answer
+    
+    return [answer,map.size]
 };
