923fe44210a7ff90b0f03733951b0f3838417c03
xion <101111364+XionWCFM@users.noreply.github.com>
2023-02-22 14:16:07 +0900

    Time: 211 ms (11.68%), Space: 48.7 MB (16.32%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -3,10 +3,7 @@
  * @return {number}
  */
 var arrayPairSum = function(nums) {
-    let answer = 0
-    nums.sort((a,b) => a - b)
-    for(i=0 ; i< nums.length ; i += 2) {
-        answer += nums[i]
-    }
-    return answer
+    
+    return nums.sort((a,b) => a-b).filter( (e,i) => i%2 == 0 ).reduce((acc,cur) => acc+cur)
+    
 };
